1ad7c9862304af104aa72aac8ebbbef93c79af51
Isaac Lima <isaac.lima25@aluno.ce.gov.br>
2024-07-28 20:21:32 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto 1m => 1 minuto");
+            Console.WriteLine("0s = Sair");
+            Console.WriteLine("Quanto tempo deseja contar ?");
+
+            string data = Console.ReadLine().ToLower();
+
+            // Verifica se a entrada é nula, vazia, não inicia com número ou é muito curta.
+            if (string.IsNullOrWhiteSpace(data) || !char.IsDigit(data[0]) || data.Length < 2)
+            {
+                Console.WriteLine("Entrada inválida. Por favor, insira um valor seguido por 's' ou 'm'");
+                Thread.Sleep(2000);
+                Menu();
+                return;
+            }
+
+            // Obtém o último caractere da entrada para determinar o tipo de unidade de tempo ('s' para segundos, 'm' para minutos).
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            // Converte a parte numérica da entrada (sem o último caractere) para um inteiro.
+            // Exemplo: se a entrada for "10s", o resultado será 10.
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+            else if (time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready ...");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Set ...");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Go ...");
+            Thread.Sleep(2500);
+
+            Start(time);
+        }
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime !=  time /*>= 0*/)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                // Console.WriteLine(time); Contagem regressiva
+                Thread.Sleep(1000);
+                // time--; Contagem regressiva
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            // Console.WriteLine("Contagem regressiva finalizada");
+            Thread.Sleep(2500);
+            Menu();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# ⏱️ Stopwatch Console Application
+
+<br>
+
+> Este projeto é um cronômetro simples desenvolvido como parte de um desafio de programação. Ele permite ao usuário iniciar uma contagem de tempo baseada em segundos ou minutos, oferecendo uma interface de linha de comando fácil de usar. A aplicação também possui uma função de contagem regressiva para ajudar os usuários a medir o tempo de forma precisa.
+
+<br>
+
+### Ajustes e melhorias
+
+O projeto ainda está em desenvolvimento, e as próximas atualizações incluirão:
+
+- [x] Implementação da contagem regressiva com entrada de tempo em segundos ou minutos
+- [x] Exibição de mensagens de contagem inicial ("Ready", "Set", "Go")
+- [ ] Melhoria na validação de entrada do usuário
+- [ ] Adição de funcionalidades adicionais, como a função de contagem em horas ("h")
+
+## 👨🏽‍💻 Tecnologias utilizadas
+
+Para o desenvolvimento deste projeto, foram utilizadas as seguintes tecnologias:
+
+- Visual Studio ou qualquer editor de código compatível com C#
+- C#
+- .NET Core (opcional)
+- Console Application
